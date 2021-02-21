31dd15f76c4c1cfffee4352db9dae5552b06ecc2
Arsiom Hontar <artemgontar16@gmail.com>
2021-02-21 19:32:08 +0300

    add Evaluator

--- src/Calculator/Controllers/CalculationsController.cs
+++ src/Calculator/Controllers/CalculationsController.cs
@@ -1,19 +1,24 @@
+using CodingSeb.ExpressionEvaluator;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace Calculator.Controllers
 {
     public class Operation
     {
-        int? result;
+        float? result;
         string expression;
 
         public Operation(string expression)
         {
             this.expression = expression;
+        }
+
+        public Operation(string expression, float result)
+        {
+            this.expression = expression;
+            this.result = result;
         }
     }
 
@@ -27,9 +32,10 @@ namespace Calculator.Controllers
     public class CalculationsController : ControllerBase
     {
         private Dictionary<int, Operation> calculations;
-
+        private ExpressionEvaluator expressionEvaluator;
         public CalculationsController()
         {
+            expressionEvaluator = new ExpressionEvaluator();
             calculations = new Dictionary<int, Operation>(){
                 {1, new Operation("2+2")},
                 {2, new Operation("4+4")}
@@ -60,10 +66,11 @@ namespace Calculator.Controllers
         /// POST api/calculations
         /// </summary>
         [HttpPost]
-        public string Post([FromBody] string expression)
+        public float Post([FromBody] string expression)
         {
-            calculations.TryAdd(calculations.Count + 1, new Operation(expression));
-            return String.Empty;
+            var result = float.Parse(expressionEvaluator.Evaluate(expression).ToString());
+            calculations.TryAdd(calculations.Count + 1, new Operation(expression, result));
+            return result;
         }
 
         /// <summary>
